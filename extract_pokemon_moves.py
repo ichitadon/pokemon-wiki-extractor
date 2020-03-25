2dc967e9fad30907224cdca4aa890a030c70b091
ichitadon <ichitadon@gmail.com>
2020-03-25 21:50:44 +0900

    わざマシン・わざレコードで覚えるわざの抽出処理でポケモンに独自フォームが存在する場合に対応した

--- extract_pokemon_moves.py
+++ extract_pokemon_moves.py
@@ -18,14 +18,14 @@ def main():
         page_name = pages[int(args[2])]['title']
         page_text = pages[int(args[2])]['revision']['text']['#text']
         pprint.pprint(extract_natural_moves(page_text, page_name))
-        # pprint.pprint(extract_machine_moves(page_text))
+        pprint.pprint(extract_machine_moves(page_text, page_name))
         # pprint.pprint(extract_egg_moves(page_text))
         # pprint.pprint(extract_tutor_moves(page_text))
 
 def extract_natural_moves(page_text: str, page_name: str):
     natural_moves_list = {}
     natural_moves_raw_text = re.search(r"==\s*レベルアップわざ\s*==(.|\s)*?==\s*\[\[わざマシン\]\]・\[\[わざレコード\]\]わざ", page_text).group()
-    # フォームが存在する場合は抽出のための印 `##` を付与する
+    # 各フォーム抽出のための印 `##` を付与する
     natural_moves_raw_text = re.sub(r"\n==", r"\n##==", natural_moves_raw_text)
     # 付与した `##` を手掛かりにフォームを抽出する
     natural_moves_text_list = re.findall(r"====\s*(.*)?\s*====((.|\s)*?)##", natural_moves_raw_text)
@@ -40,13 +40,22 @@ def extract_natural_moves(page_text: str, page_name: str):
         natural_moves_list[search_result_tuple[0]] = tmp_list
     return natural_moves_list
 
-def extract_machine_moves(page_text: str):
-    machine_moves_list = []
-    machine_moves_raw_text = re.search(r"==\s*\[\[わざマシン\]\]・\[\[わざレコード\]\]わざ\s*==(.|\s)*?== ", page_text).group()
-    for row in machine_moves_raw_text.split("\n"):
-        matched = re.search(r"\{\{learnlist/tm8\|(.*?)\|(.*?)\|", row)
-        if matched != None:
-            machine_moves_list.append(list(matched.groups()))
+def extract_machine_moves(page_text: str, page_name: str):
+    machine_moves_list = {}
+    machine_moves_raw_text = re.search(r"==\s*\[\[わざマシン\]\]・\[\[わざレコード\]\]わざ\s*==(.|\s)*?==\s*\[\[タマゴわざ\]\] ", page_text).group()
+    # 各フォーム抽出のための印 `##` を付与する
+    machine_moves_raw_text = re.sub(r"\n==", r"\n##==", machine_moves_raw_text)
+    # 付与した `##` を手掛かりにフォームを抽出する
+    machine_moves_text_list = re.findall(r"====\s*(.*)?\s*====((.|\s)*?)##", machine_moves_raw_text)
+    if len(machine_moves_text_list) == 0:
+        machine_moves_text_list.append([page_name.replace("/第八世代のおぼえるわざ", ""), machine_moves_raw_text])
+    for search_result_tuple in machine_moves_text_list:
+        tmp_list = []
+        for row in search_result_tuple[1].split("\n"):
+            matched = re.search(r"\{\{learnlist/tm8\|(.*?)\|(.*?)\|", row)
+            if matched != None:
+                tmp_list.append(list(matched.groups()))
+        machine_moves_list[search_result_tuple[0]] = tmp_list
     return machine_moves_list
 
 def extract_egg_moves(page_text: str):
